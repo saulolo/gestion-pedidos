c2564c44940c06b9444c7477ee73211db57d5fdc
Saulolo <saulolo@gmail.com>
2024-07-25 17:51:14 -0500

    :sparkles: Customización login spring security
    Detalles adicionales:
    💄 Se personaliza el formulario del login que trae por defecto spring security por uno propio agregados en los archivos css y html del login.
    🚚 Se modifica las rutas del método login y de la clase LoginController.
    ⬆️ Se actualiza la dependencia de Spring Security
    🔒️ Se modifican los métodos de la clase
    SeguridadAppConfig.

--- src/main/java/com/gestion_pedidos/config/SeguridadAppConfig.java
+++ src/main/java/com/gestion_pedidos/config/SeguridadAppConfig.java
@@ -2,77 +2,56 @@ package com.gestion_pedidos.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.SecurityBuilder;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
-/**
- * Configuración de seguridad para la aplicación.
- *
- * <p>Esta clase configura la seguridad de la aplicación utilizando usuarios en memoria.
- * Sin embargo, esta implementación es obsoleta y no se recomienda para entornos de producción.</p>
- *
- * <p><b>Nota:</b> La clase {@code WebSecurityConfigurerAdapter} ha sido deprecada en
- * Spring Security 5.7.0-M2 en favor de una configuración basada en componentes.
- * Se recomienda usar la interfaz {@code SecurityConfigurer} y la clase {@code SecurityFilterChain}
- * para configuraciones más modernas y seguras.</p>
- *
- * @deprecated Esta implementación está obsoleta y se recomienda usar la configuración basada en componentes.
- */
 @Configuration
 @EnableWebSecurity
-@Deprecated
-public class SeguridadAppConfig implements WebSecurityConfigurer {
+public class SeguridadAppConfig {
 
 
-    /**
-     * Configura un {@link InMemoryUserDetailsManager} con un usuario en memoria.
-     *
-     * <p>Este método crea un usuario con nombre de usuario "user" y contraseña "123",
-     * asignándole el rol "ADMIN".</p>
-     *
-     * @return Un {@link InMemoryUserDetailsManager} que contiene el usuario configurado.
-     */
     @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("123")
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user")
+                .password(passwordEncoder().encode("123"))
                 .roles("ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(user);
     }
 
-    /**
-     * Método init obsoleto.
-     *
-     * <p>Este método está obsoleto y no debería ser utilizado. Se recomienda
-     * utilizar la configuración basada en componentes.</p>
-     *
-     * @param builder el constructor de seguridad
-     * @throws Exception en caso de que ocurra un error durante la inicialización
-     */
-    @Override
-    @Deprecated
-    public void init(SecurityBuilder builder) throws Exception {
-        //Método deprecado
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/login", "/static/**", "/css/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin(formLogin ->
+                        formLogin
+                                .loginPage("/login")
+                                .loginProcessingUrl("/autenticacionUsuario")
+                                .permitAll()
+                )
+                .logout(LogoutConfigurer::permitAll
+                )
+                .csrf(AbstractHttpConfigurer::disable);
+
+
+        return http.build();
     }
 
-    /**
-     * Método configure obsoleto.
-     *
-     * <p>Este método está obsoleto y no debería ser utilizado. Se recomienda
-     * utilizar la configuración basada en componentes.</p>
-     *
-     * @param builder el constructor de seguridad
-     * @throws Exception en caso de que ocurra un error durante la configuración
-     */
-    @Override
-    @Deprecated
-    public void configure(SecurityBuilder builder) throws Exception {
-        //Método deprecado
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
